1d9c09fe7f8cf06f44443c0eb7e144399c15965b
JoelD7 <joeld200013@gmail.com>
2023-02-07 19:45:08 -0400

    Add loginHandler to authenticator

--- auth/authenticator/main.go
+++ auth/authenticator/main.go
@@ -1,3 +1,50 @@
 // This lambda represents the authentication server.
 // Authenticates users and generates JWTs.
 package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/JoelD7/money/api/shared/router"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
+)
+
+var (
+	errMissingUsername = errors.New("missing username")
+	errMissingPassword = errors.New("missing password")
+)
+
+func loginHandler(request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	username, ok := request.QueryStringParameters["username"]
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       "The username is required",
+		}, errMissingUsername
+	}
+
+	password := request.QueryStringParameters["password"]
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       "The password is required",
+		}, errMissingPassword
+	}
+
+	fmt.Println("username: ", username, "password: ", password)
+
+	return &events.APIGatewayProxyResponse{}, nil
+}
+
+func main() {
+	route := router.NewRouter()
+
+	route.Route("/auth", func(r *router.Router) {
+		r.Post("/login", loginHandler)
+
+	})
+
+	lambda.Start(route.Handle)
+}
