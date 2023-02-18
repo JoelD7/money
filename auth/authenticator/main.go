e3176ea0816d1eb009f8352244e6b6d6601290ff
JoelD7 <joeld200013@gmail.com>
2023-02-18 16:03:03 -0400

    Set up handler for jwks endpoint

--- auth/authenticator/main.go
+++ auth/authenticator/main.go
@@ -6,10 +6,12 @@ import (
 	"context"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"math/big"
 
 	"github.com/JoelD7/money/api/shared/router"
 	storage "github.com/JoelD7/money/api/storage/person"
@@ -52,6 +54,18 @@ type signUpBody struct {
 type Credentials struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type jwks struct {
+	Keys []jwk `json:"keys"`
+}
+
+type jwk struct {
+	Kty string `json:"kty"`
+	Kid string `json:"kid,omitempty"`
+	Use string `json:"use"`
+	N   string `json:"n"`
+	E   string `json:"e"`
 }
 
 func signUpHandler(request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
@@ -120,10 +134,35 @@ func logInHandler(request *events.APIGatewayProxyRequest) (*events.APIGatewayPro
 	}, nil
 }
 
+func jwksHandler(request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	publicKey, err := getPublicKey()
+	if err != nil {
+		return serverError(err)
+	}
+
+	response := jwks{
+		[]jwk{
+			{
+				Kty: "RSA",
+				Use: "sig",
+				N:   base64.RawURLEncoding.EncodeToString(publicKey.N.Bytes()),
+				E:   base64.RawURLEncoding.EncodeToString(big.NewInt(int64(publicKey.E)).Bytes()),
+			},
+		},
+	}
+
+	jsonResponse, err := json.Marshal(response)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(jsonResponse),
+	}, nil
+}
+
 func generateJWT(email string) (string, error) {
 	now := time.Now()
 
-	priv, pub, err := getSecretRSAKeys()
+	priv, err := getPrivateKey()
 	if err != nil {
 		return "", err
 	}
@@ -144,52 +183,56 @@ func generateJWT(email string) (string, error) {
 		return "", err
 	}
 
-	decryptingHash := jwt.NewRS256(jwt.RSAPublicKey(pub))
-	receivedPayload := &jwt.Payload{}
-
-	hd, err := jwt.Verify(token, decryptingHash, receivedPayload)
-	if err != nil {
-		return "", err
-	}
-
-	fmt.Println("Successfully verified. Header: ", hd)
+	//decryptingHash := jwt.NewRS256(jwt.RSAPublicKey(pub))
+	//receivedPayload := &jwt.Payload{}
+	//
+	//hd, err := jwt.Verify(token, decryptingHash, receivedPayload)
+	//if err != nil {
+	//	return "", err
+	//}
+	//
+	//fmt.Println("Successfully verified. Header: ", hd)
 
 	return string(token), nil
 }
 
-func getSecretRSAKeys() (*rsa.PrivateKey, *rsa.PublicKey, error) {
+func getPrivateKey() (*rsa.PrivateKey, error) {
 	privateSecret, err := secrets.GetSecret(context.Background(), privateSecretName)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	publicSecret, err := secrets.GetSecret(context.Background(), publicSecretName)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	//Create pem block
 	privatePemBlock, _ := pem.Decode([]byte(*privateSecret.SecretString))
+	fmt.Println(privatePemBlock)
 	if privatePemBlock == nil || !strings.Contains(privatePemBlock.Type, "PRIVATE KEY") {
-		return nil, nil, fmt.Errorf("failed to decode PEM private block containing private key")
+		return nil, fmt.Errorf("failed to decode PEM private block containing private key")
 	}
 
 	privateKey, err := x509.ParsePKCS1PrivateKey(privatePemBlock.Bytes)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
+	}
+
+	return privateKey, nil
+}
+
+func getPublicKey() (*rsa.PublicKey, error) {
+	publicSecret, err := secrets.GetSecret(context.Background(), publicSecretName)
+	if err != nil {
+		return nil, err
 	}
 
 	publicPemBlock, _ := pem.Decode([]byte(*publicSecret.SecretString))
-	if privatePemBlock == nil || !strings.Contains(privatePemBlock.Type, "PRIVATE KEY") {
-		return nil, nil, fmt.Errorf("failed to decode PEM public block containing public key")
+	if publicPemBlock == nil || !strings.Contains(publicPemBlock.Type, "PUBLIC KEY") {
+		return nil, fmt.Errorf("failed to decode PEM public block containing public key")
 	}
 
 	publicKey, err := x509.ParsePKIXPublicKey(publicPemBlock.Bytes)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	return privateKey, publicKey.(*rsa.PublicKey), nil
+	return publicKey.(*rsa.PublicKey), nil
 }
 
 func serverError(err error) (*events.APIGatewayProxyResponse, error) {
@@ -234,6 +277,7 @@ func main() {
 	route.Route("/auth", func(r *router.Router) {
 		r.Post("/login", logInHandler)
 		r.Post("/signup", signUpHandler)
+		r.Get("/jwks", jwksHandler)
 	})
 
 	lambda.Start(route.Handle)
