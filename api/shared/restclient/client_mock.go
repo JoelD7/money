fb136038bcb13d63a92daac44ca88a82fde942eb
JoelD7 <joeld200013@gmail.com>
2023-02-23 18:36:43 -0400

    Create restclient mock

---
+++ api/shared/restclient/client_mock.go
@@ -0,0 +1,13 @@
+package restclient
+
+import "net/http"
+
+type MockClient struct{}
+
+var (
+	GetFunction func(url string) (*http.Response, error)
+)
+
+func (m *MockClient) Get(url string) (*http.Response, error) {
+	return GetFunction(url)
+}
