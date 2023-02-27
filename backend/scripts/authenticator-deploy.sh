724ae89482868457a22226e6941cb4f38fd812e9
JoelD7 <joeld200013@gmail.com>
2023-02-27 15:44:04 -0400

    Update package path in deploy scripts

--- backend/scripts/authenticator-deploy.sh
+++ backend/scripts/authenticator-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o pipefail
-env GOOS=linux GOARCH=amd64 go build -o api/deploy/bin/authenticator/main  github.com/JoelD7/money/auth/authenticator
+env GOOS=linux GOARCH=amd64 go build -o api/deploy/bin/authenticator/main  github.com/JoelD7/money/backend/auth/authenticator
 zip -j api/deploy/bin/authenticator/main.zip api/deploy/bin/authenticator/main
 aws lambda update-function-code --function-name money-authenticator --zip-file fileb://api/deploy/bin/authenticator/main.zip
--- backend/scripts/users-deploy.sh
+++ backend/scripts/users-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o pipefail
-env GOOS=linux GOARCH=amd64 go build -o api/deploy/bin/main  github.com/JoelD7/money/api/users
+env GOOS=linux GOARCH=amd64 go build -o api/deploy/bin/main  github.com/JoelD7/money/backend/api/users
 zip -j api/deploy/bin/main.zip api/deploy/bin/main
 aws lambda update-function-code --function-name money-users-handler --zip-file fileb://api/deploy/bin/main.zip
