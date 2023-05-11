e05c8712eb94241525674d1c504e73ef61aad694
JoelD7 <joeld200013@gmail.com>
2023-05-11 18:21:55 -0400

    Separate Dynamo client's per table
    Why: So that mocks of different tables do not interfere with each other. Having a single client for all tables meant using the same mock(client) as well.

---
+++ backend/storage/person/person_mock.go
@@ -0,0 +1,131 @@
+package person
+
+import (
+	"context"
+	"fmt"
+	"github.com/JoelD7/money/backend/models"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+const dummyJWT = "header.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjo5OTk5OTk5OTk5fQ.signature"
+
+var mockedPerson *models.Person
+
+type MockDynamo struct {
+	GetItemOutput *dynamodb.GetItemOutput
+	QueryOutput   *dynamodb.QueryOutput
+	PutItemOutput *dynamodb.PutItemOutput
+
+	mockedErr error
+}
+
+func InitDynamoMock() *MockDynamo {
+	mockedPerson = GetMockedPerson()
+
+	item, err := attributevalue.MarshalMap(mockedPerson)
+	if err != nil {
+		panic(fmt.Errorf("invalid_token Dynamo mock cannot be initialized: %v", err))
+	}
+
+	mock := &MockDynamo{
+		GetItemOutput: &dynamodb.GetItemOutput{
+			Item: item,
+		},
+		QueryOutput: &dynamodb.QueryOutput{
+			Items: []map[string]types.AttributeValue{item},
+		},
+		PutItemOutput: &dynamodb.PutItemOutput{},
+		mockedErr:     nil,
+	}
+
+	DefaultClient = mock
+
+	return mock
+}
+
+// ActivateForceFailure makes any of the Dynamo operations fail with the specified error.
+// This invocation should always be followed by a deferred call to DeactivateForceFailure so that no other tests are
+// affected by this behavior.
+func (d *MockDynamo) ActivateForceFailure(err error) {
+	d.mockedErr = err
+}
+
+// DeactivateForceFailure deactivates the failures of Dynamo operations.
+func (d *MockDynamo) DeactivateForceFailure() {
+	d.mockedErr = nil
+}
+
+func (d *MockDynamo) GetItem(ctx context.Context, params *dynamodb.GetItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error) {
+	if d.mockedErr != nil {
+		return &dynamodb.GetItemOutput{}, d.mockedErr
+	}
+
+	return d.GetItemOutput, nil
+}
+
+func (d *MockDynamo) Query(ctx context.Context, params *dynamodb.QueryInput, optFns ...func(*dynamodb.Options)) (*dynamodb.QueryOutput, error) {
+	if d.mockedErr != nil {
+		return &dynamodb.QueryOutput{}, d.mockedErr
+	}
+
+	return d.QueryOutput, nil
+}
+
+func (d *MockDynamo) PutItem(ctx context.Context, params *dynamodb.PutItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error) {
+	if d.mockedErr != nil {
+		return &dynamodb.PutItemOutput{}, d.mockedErr
+	}
+
+	return d.PutItemOutput, nil
+}
+
+// MockGetItemFromSource mocks the response of the Dynamo DB's GetItem operation using source as the returned item.
+func (d *MockDynamo) MockGetItemFromSource(source interface{}) error {
+	item, err := attributevalue.MarshalMap(source)
+	if err != nil {
+		return err
+	}
+
+	d.GetItemOutput = &dynamodb.GetItemOutput{
+		Item: item,
+	}
+
+	return nil
+}
+
+// MockQueryFromSource mocks the response of the Dynamo DB's Query operation using source as the returned item.
+func (d *MockDynamo) MockQueryFromSource(source interface{}) error {
+	item, err := attributevalue.MarshalMap(source)
+	if err != nil {
+		return err
+	}
+
+	d.QueryOutput = &dynamodb.QueryOutput{
+		Items: []map[string]types.AttributeValue{item},
+	}
+
+	return nil
+}
+
+// EmptyTable makes the mocked table to be empty
+func (d *MockDynamo) EmptyTable() {
+	d.QueryOutput = &dynamodb.QueryOutput{
+		Items: []map[string]types.AttributeValue{},
+	}
+
+	d.GetItemOutput = &dynamodb.GetItemOutput{}
+}
+
+// GetMockedPerson returns the mock item for the person table
+func GetMockedPerson() *models.Person {
+	return &models.Person{
+		FullName:             "Joel",
+		Email:                "test@gmail.com",
+		Password:             "$2a$10$.THF8QG33va8JTSIBz3lPuULaO6NiDb6yRmew63OtzujhVHbnZMFe",
+		PreviousRefreshToken: "previous token",
+		AccessToken:          dummyJWT,
+		RefreshToken:         dummyJWT,
+	}
+}
