8c218b81441d3596750f68e789322653333bec65
JoelD7 <joeld200013@gmail.com>
2023-05-13 10:04:27 -0400

    Add creation date and type fields to invalid_token model
    Why: Improve visibility and maintainability of the code

--- backend/models/invalid_token.go
+++ backend/models/invalid_token.go
@@ -1,7 +1,11 @@
 package models
 
+import "time"
+
 type InvalidToken struct {
-	Email  string `json:"email,omitempty" dynamodbav:"email"`
-	Token  string `json:"token,omitempty" dynamodbav:"token"`
-	Expire int64  `json:"expire,omitempty" dynamodbav:"expire"`
+	Email       string    `json:"email,omitempty" dynamodbav:"email"`
+	Token       string    `json:"token,omitempty" dynamodbav:"token"`
+	Expire      int64     `json:"expire,omitempty" dynamodbav:"expire"`
+	Type        string    `json:"type,omitempty" dynamodbav:"type"`
+	CreatedDate time.Time `json:"created_date,omitempty" dynamodbav:"created_date,omitempty"`
 }
