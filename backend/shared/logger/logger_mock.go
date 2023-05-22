febc26fa36ce3519c7399f06fa305d974f9c9f64
JoelD7 <joeld200013@gmail.com>
2023-05-22 19:32:23 -0400

    Reset logger mock's buffer inside mock methods
    Why: Prevent the client from having to do that himself. It might be error-prone

--- backend/shared/logger/logger_mock.go
+++ backend/shared/logger/logger_mock.go
@@ -6,13 +6,13 @@ import (
 )
 
 type LogMock struct {
-	Output bytes.Buffer
+	Output *bytes.Buffer
 }
 
 // InitLoggerMock mocks the logger client. This is important to prevent unit tests from sending logs.
 func InitLoggerMock(buf bytes.Buffer) *LogMock {
 	logMock := &LogMock{
-		Output: buf,
+		Output: &buf,
 	}
 
 	LogClient = logMock
@@ -21,15 +21,19 @@ func InitLoggerMock(buf bytes.Buffer) *LogMock {
 }
 
 func (l *LogMock) Info(eventName string, objects []Object) {
+	l.Output.Reset()
 	_, _ = l.Output.Write([]byte(eventName))
 }
 func (l *LogMock) Warning(eventName string, err error, objects []Object) {
+	l.Output.Reset()
 	_, _ = l.Output.Write([]byte(eventName))
 }
 func (l *LogMock) Error(eventName string, err error, objects []Object) {
+	l.Output.Reset()
 	_, _ = l.Output.Write([]byte(eventName))
 }
 func (l *LogMock) Critical(eventName string, objects []Object) {
+	l.Output.Reset()
 	_, _ = l.Output.Write([]byte(eventName))
 }
 func (*LogMock) LogLambdaTime(startingTime time.Time, err error, panic interface{}) {
