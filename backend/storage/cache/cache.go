83f186d28dc0067a184b766e2c4b34092dea15cb
JoelD7 <joeld200013@gmail.com>
2023-08-14 20:13:25 -0400

    Start work on abstracting redis cache repository
    - Renamed files
    - Created interface

--- backend/storage/cache/cache.go
+++ backend/storage/cache/cache.go
@@ -2,18 +2,7 @@ package cache
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"github.com/JoelD7/money/backend/models"
-	"github.com/JoelD7/money/backend/shared/utils"
-	"github.com/JoelD7/money/backend/storage/invalidtoken"
-	"time"
-)
-
-var (
-	ErrTokensNotFound = errors.New("no invalid tokens found")
-	ErrInvalidTTL     = errors.New("TTL is from a past datetime")
-	ErrNoSuchKey      = errors.New("key does not exist")
 )
 
 const (
@@ -21,96 +10,23 @@ const (
 	keyPrefix = "invalid_tokens:"
 )
 
-func GetInvalidTokens(ctx context.Context, email string) ([]*models.InvalidToken, error) {
-	key := keyPrefix + email
-
-	dataStr, err := redisClient.Get(ctx, key)
-	if errors.Is(err, ErrNotFound) {
-		return nil, ErrTokensNotFound
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	invalidTokens := make([]*models.InvalidToken, 0)
-
-	err = json.Unmarshal([]byte(dataStr), &invalidTokens)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(invalidTokens) == 0 {
-		return nil, ErrTokensNotFound
-	}
-
-	return invalidTokens, nil
+type repositoryAPI interface {
+	getInvalidTokens(ctx context.Context, email string) ([]*models.InvalidToken, error)
+	addInvalidToken(ctx context.Context, email, token string, ttl int64) error
 }
 
-func AddInvalidToken(ctx context.Context, email, token string, ttl int64) error {
-	if time.Now().Unix() > ttl && ttl > 0 {
-		return ErrInvalidTTL
-	}
-
-	key := keyPrefix + email
-
-	invalidTokens, err := GetInvalidTokens(ctx, email)
-	if err != nil && !errors.Is(err, ErrTokensNotFound) {
-		return err
-	}
-
-	newInvalidTokens := make([]*models.InvalidToken, 0)
-	newInvalidTokens = append(newInvalidTokens, &models.InvalidToken{Token: token, Expire: ttl, CreatedDate: time.Now()})
-
-	now := time.Now().Unix()
-
-	for _, it := range invalidTokens {
-		if now <= it.Expire {
-			newInvalidTokens = append(newInvalidTokens, it)
-		}
-	}
-
-	result, err := utils.GetJsonString(newInvalidTokens)
-	if err != nil {
-		return err
-	}
-
-	err = redisClient.Set(ctx, key, result, 0)
-	if err != nil {
-		return err
-	}
-
-	return nil
+type Repository struct {
+	client repositoryAPI
 }
 
-func Delete(ctx context.Context, key string) error {
-	result, err := client.Del(ctx, key).Result()
-	if err != nil {
-		return err
-	}
-
-	if result == 0 {
-		return ErrNoSuchKey
-	}
-
-	return nil
+func NewRepository(client repositoryAPI) *Repository {
+	return &Repository{client}
 }
 
-func migrateTokens() {
-	ctx := context.Background()
+func (r *Repository) GetInvalidTokens(ctx context.Context, email string) ([]*models.InvalidToken, error) {
+	return r.client.getInvalidTokens(ctx, email)
+}
 
-	invalidTokens, err := invalidtoken.GetAllForPerson(ctx, email)
-	if err != nil {
-		panic(err)
-	}
-
-	result, err := utils.GetJsonString(invalidTokens)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = client.SetNX(ctx, "invalid_tokens:test@gmail.com", result, 0).Result()
-	if err != nil {
-		panic(err)
-	}
+func (r *Repository) AddInvalidToken(ctx context.Context, email, token string, ttl int64) error {
+	return r.client.addInvalidToken(ctx, email, token, ttl)
 }
---
+++ backend/storage/cache/redis_cache.go
@@ -0,0 +1,84 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"time"
+
+	"github.com/JoelD7/money/backend/models"
+	"github.com/JoelD7/money/backend/shared/utils"
+)
+
+var (
+	ErrTokensNotFound = errors.New("no invalid tokens found")
+	ErrInvalidTTL     = errors.New("TTL is from a past datetime")
+)
+
+type redisCache struct{}
+
+func NewRedisCache() *redisCache {
+	return &redisCache{}
+}
+
+func (r *redisCache) getInvalidTokens(ctx context.Context, email string) ([]*models.InvalidToken, error) {
+	key := keyPrefix + email
+
+	dataStr, err := redisClient.Get(ctx, key)
+	if errors.Is(err, ErrNotFound) {
+		return nil, ErrTokensNotFound
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	invalidTokens := make([]*models.InvalidToken, 0)
+
+	err = json.Unmarshal([]byte(dataStr), &invalidTokens)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(invalidTokens) == 0 {
+		return nil, ErrTokensNotFound
+	}
+
+	return invalidTokens, nil
+}
+
+func (r *redisCache) addInvalidToken(ctx context.Context, email, token string, ttl int64) error {
+	if time.Now().Unix() > ttl && ttl > 0 {
+		return ErrInvalidTTL
+	}
+
+	key := keyPrefix + email
+
+	invalidTokens, err := r.getInvalidTokens(ctx, email)
+	if err != nil && !errors.Is(err, ErrTokensNotFound) {
+		return err
+	}
+
+	newInvalidTokens := make([]*models.InvalidToken, 0)
+	newInvalidTokens = append(newInvalidTokens, &models.InvalidToken{Token: token, Expire: ttl, CreatedDate: time.Now()})
+
+	now := time.Now().Unix()
+
+	for _, it := range invalidTokens {
+		if now <= it.Expire {
+			newInvalidTokens = append(newInvalidTokens, it)
+		}
+	}
+
+	result, err := utils.GetJsonString(newInvalidTokens)
+	if err != nil {
+		return err
+	}
+
+	err = redisClient.Set(ctx, key, result, 0)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
