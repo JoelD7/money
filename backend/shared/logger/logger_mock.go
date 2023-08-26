1d624284a0faead989b2f66bb03f4ee33fd4d506
JoelD7 <joeld200013@gmail.com>
2023-08-26 16:18:02 -0400

    Add MapToLoggerObject to LogMock type
    Why: Added this method to the Log interface so that a log object can be injectable

--- backend/shared/logger/logger_mock.go
+++ backend/shared/logger/logger_mock.go
@@ -2,11 +2,19 @@ package logger
 
 import (
 	"bytes"
+	"github.com/JoelD7/money/backend/models"
 	"time"
 )
 
 type LogMock struct {
 	Output *bytes.Buffer
+}
+
+func (l *LogMock) MapToLoggerObject(name string, m map[string]interface{}) models.LoggerObject {
+	return &ObjectWrapper{
+		name:       name,
+		properties: m,
+	}
 }
 
 // InitLoggerMock mocks the logger client. This is important to prevent unit tests from sending logs.
@@ -24,19 +32,19 @@ func InitLoggerMock(buf *bytes.Buffer) *LogMock {
 	return logMock
 }
 
-func (l *LogMock) Info(eventName string, objects []Object) {
+func (l *LogMock) Info(eventName string, objects ...models.LoggerObject) {
 	_, _ = l.Output.Write([]byte(eventName))
 }
-func (l *LogMock) Warning(eventName string, err error, objects []Object) {
+func (l *LogMock) Warning(eventName string, err error, objects ...models.LoggerObject) {
 	_, _ = l.Output.Write([]byte(eventName))
 	_, _ = l.Output.Write([]byte(err.Error()))
 }
-func (l *LogMock) Error(eventName string, err error, objects []Object) {
+func (l *LogMock) Error(eventName string, err error, objects ...models.LoggerObject) {
 	_, _ = l.Output.Write([]byte(eventName))
 	_, _ = l.Output.Write([]byte(err.Error()))
 }
 
-func (l *LogMock) Critical(eventName string, objects []Object) {
+func (l *LogMock) Critical(eventName string, objects ...models.LoggerObject) {
 	_, _ = l.Output.Write([]byte(eventName))
 }
 func (*LogMock) LogLambdaTime(startingTime time.Time, err error, panic interface{}) {}
