2a53e39b6c3bac076784faa29c8c392d50e11d9d
JoelD7 <joeld200013@gmail.com>
2023-09-01 19:22:08 -0400

    Change scripts to build lambda functions using new architecture arm64
    Why: AWS will deprecate the go1.x runtime(https://aws.amazon.com/blogs/compute/migrating-aws-lambda-functions-from-the-go1-x-runtime-to-the-custom-runtime-on-amazon-linux-2/)

--- backend/scripts/lambda-authorizer-deploy.sh
+++ backend/scripts/lambda-authorizer-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o pipefail
-env GOOS=linux GOARCH=amd64 go build -o api/deploy/bin/lambda-authorizer/main  github.com/JoelD7/money/backend/auth/lambda-authorizer
+env GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o api/deploy/bin/lambda-authorizer/main  github.com/JoelD7/money/backend/auth/lambda-authorizer
 zip -j api/deploy/bin/lambda-authorizer/main.zip api/deploy/bin/lambda-authorizer/main
 aws lambda update-function-code --function-name money-lambda-authorizer --zip-file fileb://api/deploy/bin/lambda-authorizer/main.zip
