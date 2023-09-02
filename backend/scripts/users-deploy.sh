6a595f4d45a63c7d9c7f4b63439c1b0a23d38927
JoelD7 <joeld200013@gmail.com>
2023-09-01 20:57:31 -0400

    Use "bootstrap" as executable name for lambda functions
    Why: AWS expects the executables to be named like this

--- backend/scripts/users-deploy.sh
+++ backend/scripts/users-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o pipefail
-env GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o api/deploy/bin/main  github.com/JoelD7/money/backend/api/functions/users
-zip -j api/deploy/bin/main.zip api/deploy/bin/main
-aws lambda update-function-code --function-name money-users-handler --zip-file fileb://api/deploy/bin/main.zip
+env GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o api/deploy/bin/bootstrap  github.com/JoelD7/money/backend/api/functions/users
+zip -j api/deploy/bin/bootstrap.zip api/deploy/bin/bootstrap
+aws lambda update-function-code --function-name money-users-handler --zip-file fileb://api/deploy/bin/bootstrap.zip
