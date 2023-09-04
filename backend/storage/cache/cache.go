26a6bb664f7c100242287225cd61304a6a51874e
JoelD7 <joeld200013@gmail.com>
2023-09-04 19:29:09 -0400

    Implement InvalidTokenManager interface in redis cache type directly
    Why: The previous "Repository" implementation was adding an unnecesary/confusing layer of abstraction and made the lambda-authorizer controller depend on a type.

--- backend/storage/cache/cache.go
+++ backend/storage/cache/cache.go
@@ -9,23 +9,7 @@ const (
 	keyPrefix = "invalid_tokens:"
 )
 
-type InvalidTokenCache interface {
-	getInvalidTokens(ctx context.Context, email string) ([]*models.InvalidToken, error)
-	addInvalidToken(ctx context.Context, email, token string, ttl int64) error
-}
-
-type Repository struct {
-	client InvalidTokenCache
-}
-
-func NewRepository(client InvalidTokenCache) *Repository {
-	return &Repository{client}
-}
-
-func (r *Repository) GetInvalidTokens(ctx context.Context, email string) ([]*models.InvalidToken, error) {
-	return r.client.getInvalidTokens(ctx, email)
-}
-
-func (r *Repository) AddInvalidToken(ctx context.Context, email, token string, ttl int64) error {
-	return r.client.addInvalidToken(ctx, email, token, ttl)
+type InvalidTokenManager interface {
+	GetInvalidTokens(ctx context.Context, email string) ([]*models.InvalidToken, error)
+	AddInvalidToken(ctx context.Context, email, token string, ttl int64) error
 }
