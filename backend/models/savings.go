f850dbdfd59aee8f6529d637d57d82202089771c
JoelD7 <joeld200013@gmail.com>
2023-09-19 19:59:08 -0400

    Add "period_user" attribute to Saving model

--- backend/models/savings.go
+++ backend/models/savings.go
@@ -6,6 +6,7 @@ type Saving struct {
 	SavingID     string    `json:"saving_id,omitempty"  dynamodbav:"saving_id"`
 	SavingGoalID string    `json:"saving_goal_id,omitempty"  dynamodbav:"saving_goal_id"`
 	Username     string    `json:"username,omitempty"  dynamodbav:"username"`
+	PeriodUser   string    `json:"period_user,omitempty"  dynamodbav:"period_user"`
 	CreatedDate  time.Time `json:"created_date,omitempty"  dynamodbav:"created_date"`
 	UpdatedDate  time.Time `json:"updated_date,omitempty"  dynamodbav:"updated_date"`
 	Amount       float64   `json:"amount" dynamodbav:"amount"`
