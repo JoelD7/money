0c04c4ccbdbbc91b6dda5eaac530ade356538e28
JoelD7 <joeld200013@gmail.com>
2023-09-20 20:03:32 -0400

    Change SavingGoal to be it's own entity/table
    Why: It makes more sense from a query standpoint to have a SavingGoal separate from a User entity. See https://www.notion.so/Saving-goal-cf745386940647eda9f9fa8e1efbc4df?pvs=4 for details.

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,17 +3,16 @@ package models
 import "time"
 
 type User struct {
-	FullName      string        `json:"full_name,omitempty" dynamodbav:"full_name,omitempty"`
-	Username      string        `json:"username,omitempty" dynamodbav:"username"`
-	Password      string        `json:"-" dynamodbav:"password"`
-	Categories    []*Category   `json:"categories,omitempty" dynamodbav:"categories,omitempty"`
-	SavingGoals   []*SavingGoal `json:"saving_goals,omitempty" dynamodbav:"saving_goals,omitempty"`
-	CreatedDate   time.Time     `json:"created_date,omitempty" dynamodbav:"created_date,omitempty"`
-	UpdatedDate   time.Time     `json:"updated_date,omitempty" dynamodbav:"update_date,omitempty"`
-	AccessToken   string        `json:"-" dynamodbav:"access_token,omitempty"`
-	RefreshToken  string        `json:"-" dynamodbav:"refresh_token"`
-	CurrentPeriod string        `json:"current_period,omitempty" dynamodbav:"current_period,omitempty"`
-	Remainder     float64       `json:"remainder,omitempty" dynamodbav:"remainder,omitempty"`
+	FullName      string      `json:"full_name,omitempty" dynamodbav:"full_name,omitempty"`
+	Username      string      `json:"username,omitempty" dynamodbav:"username"`
+	Password      string      `json:"-" dynamodbav:"password"`
+	Categories    []*Category `json:"categories,omitempty" dynamodbav:"categories,omitempty"`
+	CreatedDate   time.Time   `json:"created_date,omitempty" dynamodbav:"created_date,omitempty"`
+	UpdatedDate   time.Time   `json:"updated_date,omitempty" dynamodbav:"update_date,omitempty"`
+	AccessToken   string      `json:"-" dynamodbav:"access_token,omitempty"`
+	RefreshToken  string      `json:"-" dynamodbav:"refresh_token"`
+	CurrentPeriod string      `json:"current_period,omitempty" dynamodbav:"current_period,omitempty"`
+	Remainder     float64     `json:"remainder,omitempty" dynamodbav:"remainder,omitempty"`
 }
 
 type Category struct {
@@ -29,13 +28,6 @@ type Subcategory struct {
 	SubcategoryID   string `json:"subcategory_id,omitempty" dynamodbav:"subcategory_id"`
 	SubcategoryName string `json:"subcategory_name,omitempty" dynamodbav:"subcategory_name,omitempty"`
 	Color           string `json:"color,omitempty" dynamodbav:"color,omitempty"`
-}
-
-type SavingGoal struct {
-	SavingGoalID string    `json:"saving_goal_id,omitempty" dynamodbav:"saving_goal_id,omitempty"`
-	Name         string    `json:"name,omitempty" dynamodbav:"name,omitempty"`
-	Goal         float64   `json:"goal,omitempty" dynamodbav:"goal,omitempty"`
-	Deadline     time.Time `json:"deadline,omitempty" dynamodbav:"deadline,omitempty"`
 }
 
 func (u User) LogName() string {
