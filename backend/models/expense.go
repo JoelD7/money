8b6ca93b6f5232833c88e6766e7edb295af27b78
JoelD7 <joeld200013@gmail.com>
2023-09-30 16:29:52 -0400

    Remove savingGoalID from Expense entity
    Why: That attribute now belongs to the Saving entity

--- backend/models/expense.go
+++ backend/models/expense.go
@@ -3,15 +3,14 @@ package models
 import "time"
 
 type Expense struct {
-	ExpenseID    string    `json:"expense_id"`
-	Username     string    `json:"username,omitempty"`
-	CategoryID   string    `json:"category_id,omitempty"`
-	SavingGoalID string    `json:"saving_goal_id,omitempty"`
-	Amount       float64   `json:"amount"`
-	Currency     string    `json:"currency,omitempty"`
-	Name         string    `json:"name,omitempty"`
-	Notes        string    `json:"notes,omitempty"`
-	Date         time.Time `json:"date,omitempty"`
-	Period       string    `json:"period,omitempty"`
-	UpdateDate   time.Time `json:"update_date,omitempty"`
+	ExpenseID  string    `json:"expense_id"`
+	Username   string    `json:"username,omitempty"`
+	CategoryID string    `json:"category_id,omitempty"`
+	Amount     float64   `json:"amount"`
+	Currency   string    `json:"currency,omitempty"`
+	Name       string    `json:"name,omitempty"`
+	Notes      string    `json:"notes,omitempty"`
+	Date       time.Time `json:"date,omitempty"`
+	Period     string    `json:"period,omitempty"`
+	UpdateDate time.Time `json:"update_date,omitempty"`
 }
