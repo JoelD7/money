c0d54b8d218aecdf0eda63b5396146294fbe79d9
JoelD7 <joeld200013@gmail.com>
2023-10-07 14:18:21 -0400

    Remove omitempty from PeriodUser attribute in Saving model
    Why: Despite having the "-", the attribute was not being ignored in the json unmarshal because it had the "omitempty"

--- backend/models/savings.go
+++ backend/models/savings.go
@@ -8,7 +8,7 @@ type Saving struct {
 	SavingGoalName string    `json:"saving_goal_name,omitempty"`
 	Username       string    `json:"username,omitempty"`
 	Period         string    `json:"period,omitempty"`
-	PeriodUser     string    `json:"-,omitempty"`
+	PeriodUser     string    `json:"-"`
 	CreatedDate    time.Time `json:"created_date,omitempty"`
 	UpdatedDate    time.Time `json:"updated_date,omitempty"`
 	Amount         float64   `json:"amount"`
