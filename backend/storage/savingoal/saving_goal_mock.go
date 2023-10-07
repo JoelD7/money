3f73fb38eff939dc145f4281f3905169f019d17c
JoelD7 <joeld200013@gmail.com>
2023-10-07 13:57:47 -0400

    Implement GetSavingGoals in saving_goal_mock.go

--- backend/storage/savingoal/saving_goal_mock.go
+++ backend/storage/savingoal/saving_goal_mock.go
@@ -34,5 +34,20 @@ func (m *Mock) GetSavingGoal(ctx context.Context, username, savingGoalID string)
 		Goal:         1500,
 		Deadline:     time.Now().Add(time.Hour * 24 * 30 * 6),
 	}, nil
+}
 
+func (m *Mock) GetSavingGoals(ctx context.Context, username string) ([]*models.SavingGoal, error) {
+	if m.mockedErr != nil {
+		return nil, m.mockedErr
+	}
+
+	return []*models.SavingGoal{
+		{
+			Username:     username,
+			SavingGoalID: "savingGoalID",
+			Name:         "mocked_name",
+			Goal:         1500,
+			Deadline:     time.Now().Add(time.Hour * 24 * 30 * 6),
+		},
+	}, nil
 }
