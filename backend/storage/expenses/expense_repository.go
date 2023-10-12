13635f4d123d9e9080d2c9252dea59965781ad5c
JoelD7 <joeld200013@gmail.com>
2023-10-11 20:40:07 -0400

    Add methods of GetExpenses and GetExpensesByPeriodAndCategories to expenses repository

--- backend/storage/expenses/expense_repository.go
+++ backend/storage/expenses/expense_repository.go
@@ -13,5 +13,7 @@ type DynamoAPI interface {
 }
 
 type Repository interface {
-	GetExpensesByPeriod(ctx context.Context, username, periodID string) ([]*models.Expense, error)
+	GetExpenses(ctx context.Context, username, startKey string, pageSize int) ([]*models.Expense, string, error)
+	GetExpensesByPeriod(ctx context.Context, username, periodID, startKey string, pageSize int) ([]*models.Expense, string, error)
+	GetExpensesByPeriodAndCategories(ctx context.Context, username, periodID, startKey string, categories []string, pageSize int) ([]*models.Expense, string, error)
 }
