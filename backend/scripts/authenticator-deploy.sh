6d575547ef81be396d4c2b7a10353b2f0db390bb
JoelD7 <joeld200013@gmail.com>
2023-10-19 18:01:56 -0400

    Add "| tee" to deployment scripts
    Why: So that the output of the deployment commands doesn't truncate

--- backend/scripts/authenticator-deploy.sh
+++ backend/scripts/authenticator-deploy.sh
@@ -2,4 +2,4 @@
 set -o pipefail
 env GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o auth/deploy/bin/authenticator/bootstrap  github.com/JoelD7/money/backend/auth/authenticator
 zip -j auth/deploy/bin/authenticator/bootstrap.zip auth/deploy/bin/authenticator/bootstrap
-aws lambda update-function-code --function-name money-authenticator --zip-file fileb://auth/deploy/bin/authenticator/bootstrap.zip
+aws lambda update-function-code --function-name money-authenticator --zip-file fileb://auth/deploy/bin/authenticator/bootstrap.zip | tee
--- backend/scripts/expenses-deploy.sh
+++ backend/scripts/expenses-deploy.sh
@@ -2,4 +2,4 @@
 set -o pipefail
 GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o api/deploy/bin/expenses/bootstrap github.com/JoelD7/money/backend/api/functions/expenses
 zip -j api/deploy/bin/expenses/bootstrap.zip api/deploy/bin/expenses/bootstrap
-aws lambda update-function-code --function-name money-expenses-handler --zip-file fileb://api/deploy/bin/expenses/bootstrap.zip
+aws lambda update-function-code --function-name money-expenses-handler --zip-file fileb://api/deploy/bin/expenses/bootstrap.zip | tee
