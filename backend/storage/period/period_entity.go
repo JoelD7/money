fef10100fad768bc4df8023b1ed776ca59a41d31
JoelD7 <joeld200013@gmail.com>
2023-10-21 11:13:44 -0400

    Add startDate and endDate to period model and entity

--- backend/storage/period/period_entity.go
+++ backend/storage/period/period_entity.go
@@ -9,6 +9,8 @@ type periodEntity struct {
 	Username    string    `json:"username,omitempty" dynamodbav:"username"`
 	ID          string    `json:"period_id,omitempty" dynamodbav:"period_id"`
 	Name        string    `json:"name,omitempty" dynamodbav:"name"`
+	StartDate   time.Time `json:"start_date,omitempty" dynamodbav:"start_date"`
+	EndDate     time.Time `json:"end_date,omitempty" dynamodbav:"end_date"`
 	CreatedDate time.Time `json:"created_date,omitempty" dynamodbav:"created_date"`
 	UpdatedDate time.Time `json:"updated_date,omitempty" dynamodbav:"updated_date"`
 }
@@ -18,6 +20,8 @@ func toPeriodModel(p *periodEntity) *models.Period {
 		Username:    p.Username,
 		ID:          p.ID,
 		Name:        p.Name,
+		StartDate:   p.StartDate,
+		EndDate:     p.EndDate,
 		CreatedDate: p.CreatedDate,
 		UpdatedDate: p.UpdatedDate,
 	}
@@ -38,6 +42,8 @@ func toPeriodEntity(period *models.Period) *periodEntity {
 		Username:    period.Username,
 		ID:          period.ID,
 		Name:        period.Name,
+		StartDate:   period.StartDate,
+		EndDate:     period.EndDate,
 		CreatedDate: period.CreatedDate,
 		UpdatedDate: period.UpdatedDate,
 	}
