14d9f2dc540e0e22159713243c48bbed2a5277aa
JoelD7 <joeld200013@gmail.com>
2023-11-04 17:00:08 -0400

    Remove PeriodTime type
    Why: Review decision log about this

--- backend/models/period.go
+++ backend/models/period.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"fmt"
-	"strings"
 	"time"
 )
 
@@ -14,51 +12,6 @@ type Period struct {
 	EndDate     time.Time `json:"end_date,omitempty"`
 	CreatedDate time.Time `json:"created_date,omitempty"`
 	UpdatedDate time.Time `json:"updated_date,omitempty"`
-}
-
-type PeriodTime struct {
-	time.Time
-}
-
-const ctLayout = "2006-01-02"
-
-func ToTime(p PeriodTime) time.Time {
-	return p.Time
-}
-
-func ToPeriodTime(t time.Time) PeriodTime {
-	return PeriodTime{t}
-}
-
-func (ct *PeriodTime) UnmarshalJSON(b []byte) error {
-	s := strings.Trim(string(b), "\"")
-	if s == "null" {
-		ct.Time = time.Time{}
-		return nil
-	}
-
-	var err error
-
-	ct.Time, err = time.Parse(ctLayout, s)
-	if err != nil {
-		return fmt.Errorf("cannot unmarshal %s into a time.Time: %w", b, err)
-	}
-
-	return err
-}
-
-func (ct *PeriodTime) MarshalJSON() ([]byte, error) {
-	if ct.Time.IsZero() {
-		return []byte("null"), nil
-	}
-
-	return []byte(fmt.Sprintf("\"%s\"", ct.Time.Format(ctLayout))), nil
-}
-
-//var nilTime = (time.Time{}).UnixNano()
-
-func (ct *PeriodTime) IsSet() bool {
-	return !ct.IsZero()
 }
 
 func (period *Period) LogName() string {
