dd49c9e218f954f6d28bc71b48c0eed77817c26a
JoelD7 <joeld200013@gmail.com>
2023-11-08 20:18:00 -0400

    Implement usecase method to delete period

--- backend/usecases/period.go
+++ backend/usecases/period.go
@@ -17,6 +17,7 @@ type PeriodManager interface {
 	GetPeriod(ctx context.Context, username, period string) (*models.Period, error)
 	GetLastPeriod(ctx context.Context, username string) (*models.Period, error)
 	GetPeriods(ctx context.Context, username, startKey string, pageSize int) ([]*models.Period, string, error)
+	DeletePeriod(ctx context.Context, periodID, username string) error
 }
 
 func NewPeriodCreator(pm PeriodManager, log Logger) func(ctx context.Context, username string, period *models.Period) (*models.Period, error) {
@@ -67,5 +68,11 @@ func NewPeriodGetter(pm PeriodManager) func(ctx context.Context, username, perio
 func NewPeriodsGetter(pm PeriodManager) func(ctx context.Context, username, startKey string, pageSize int) ([]*models.Period, string, error) {
 	return func(ctx context.Context, username, startKey string, pageSize int) ([]*models.Period, string, error) {
 		return pm.GetPeriods(ctx, username, startKey, pageSize)
+	}
+}
+
+func NewPeriodDeleter(pm PeriodManager) func(ctx context.Context, periodID, username string) error {
+	return func(ctx context.Context, periodID, username string) error {
+		return pm.DeletePeriod(ctx, periodID, username)
 	}
 }
