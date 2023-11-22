12606fbe1cdd437da6ca82f6ea3fa70bae94a39b
JoelD7 <joeld200013@gmail.com>
2023-11-22 19:28:10 -0400

    [income] Use shared encode/decode functions for last evaluated key
    Why:
     - Because those functions aren't dependent on the last evaluated key of the table being queried, they are now located on a shared package among the repositories

--- backend/shared/logger/logger.go
+++ backend/shared/logger/logger.go
@@ -30,7 +30,7 @@ const (
 
 var (
 	logstashServerType = env.GetString("LOGSTASH_TYPE", "tcp")
-	logstashHost       = env.GetString("LOGSTASH_HOST", "ec2-3-90-36-106.compute-1.amazonaws.com")
+	logstashHost       = env.GetString("LOGSTASH_HOST", "ec2-54-145-42-15.compute-1.amazonaws.com")
 	logstashPort       = env.GetString("LOGSTASH_PORT", "5044")
 
 	stackCleaner = regexp.MustCompile(`[^\t]*:\d+`)
