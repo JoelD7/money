61d5dcbcd5e9794ea9ba70f85eb6363b5ddcb58a
JoelD7 <joeld200013@gmail.com>
2023-11-30 20:24:09 -0400

    Setup tailwindcss

---
+++ frontend/src/atoms/Button.tsx
@@ -0,0 +1,11 @@
+import "tailwindcss/tailwind.css";
+
+export function Button() {
+    return (
+        <>
+            <button className="bg-red-500">
+                Test buttons
+            </button>
+        </>
+    );
+}
--- frontend/src/dev/previews.tsx
+++ frontend/src/dev/previews.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import {Previews} from "@react-buddy/ide-toolbox";
+import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
+import {Button} from "../atoms/Button.tsx";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Button">
+                <Button/>
+            </ComponentPreview>
         </Previews>
     );
 };
