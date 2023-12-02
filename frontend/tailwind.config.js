61bb864d4af801f951515a1079abbbc8caaccf99
JoelD7 <joeld200013@gmail.com>
2023-12-02 14:11:43 -0400

    Add project colors to theme palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,20 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      green: '#009821',
+      orange: '#FF8042',
+      red: '#D90707',
+      darkGreen: '#024511',
+      lightBlue: '#0088FE',
+      gray:{
+        100: '#D9D9D9',
+        200: '#6F6F6F',
+      }
+    },
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif'],
+    }
   },
   plugins: [],
 }
