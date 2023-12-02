ed10f8ba12b0c9cd4389f35928f9a7ee99045a41
JoelD7 <joeld200013@gmail.com>
2023-12-02 14:58:17 -0400

    Add color classes and types to Button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import {useState} from 'react'
 import './App.css'
-import {Button} from "./atoms/Button.tsx";
+import {Button, ButtonColor} from "./atoms/Button.tsx";
 import "tailwindcss/tailwind.css";
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
-            <Button text={"Button"}></Button>
+            <Button color={ButtonColor.White}></Button>
         </>
     )
 }
