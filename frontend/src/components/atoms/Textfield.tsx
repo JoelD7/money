f6c29420045c69ca7665273a05f43b73272dc3f9
JoelD7 <joeld200013@gmail.com>
2023-12-03 14:19:04 -0400

    Make label "display: block" in textfield component

--- frontend/src/components/atoms/Textfield.tsx
+++ frontend/src/components/atoms/Textfield.tsx
@@ -7,11 +7,9 @@ type TextfieldProps = {
 export function Textfield({label = "Label", name}: TextfieldProps) {
     return (
         <>
-            <div className={"block"}>
-                <label htmlFor={`${name}`} className="text-gray-200">{label}</label>
-                <input type="text" id={name} name={name} className="border-2  border-gray-100 rounded-md p-2 m-2"
-                       placeholder="Text"/>
-            </div>
+            <label htmlFor={`${name}`} className="text-gray-200 block">{label}</label>
+            <input type="text" id={name} name={name} className="border-2  border-gray-100 rounded-md p-2 m-2"
+                   placeholder="Text"/>
         </>
     );
 }
