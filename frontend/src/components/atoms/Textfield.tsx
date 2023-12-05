e6c0b5a161cfb5724609a58162ba92e1f3d06b21
JoelD7 <joeld200013@gmail.com>
2023-12-04 20:16:22 -0400

    Finish creation of textfield component

--- frontend/src/components/atoms/Textfield.tsx
+++ frontend/src/components/atoms/Textfield.tsx
@@ -8,7 +8,7 @@ export function Textfield({label = "Label", name}: TextfieldProps) {
     return (
         <>
             <label htmlFor={`${name}`} className="text-gray-200 block">{label}</label>
-            <input type="text" id={name} name={name} className="border-2  border-gray-100 rounded-md p-2 m-2"
+            <input type="text" id={name} name={name} className="border-2 border-gray-100 rounded-lg p-2"
                    placeholder="Text"/>
         </>
     );
