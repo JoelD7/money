5774960c3239521f7eb035a45c16758516e2741f
JoelD7 <joeld200013@gmail.com>
2023-12-07 18:47:22 -0400

    Finish creation of textarea component

--- frontend/src/components/atoms/Textarea.tsx
+++ frontend/src/components/atoms/Textarea.tsx
@@ -1,9 +1,14 @@
-export function Textarea() {
+type TextareaProps = {
+    name: string;
+    label?: string;
+};
+
+export function Textarea({label = "Label", name}: TextareaProps) {
     return (
         <>
             <label htmlFor={`${name}`} className="text-gray-200 block">{label}</label>
-            <input type="text" id={name} name={name} className="border-2 border-gray-100 rounded-lg p-2"
-                   placeholder="Text"/>
+            <textarea name={name} id={name} cols="30" rows="5" placeholder="Text"
+                      className="border-2 border-gray-100 rounded-lg p-2"></textarea>
         </>
     );
 }
