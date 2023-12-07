ae6aba741be668e7bade11488a7b55579f75215b
JoelD7 <joeld200013@gmail.com>
2023-12-06 20:24:40 -0400

    Add initial logic for textarea component

--- frontend/src/components/atoms/Textarea.tsx
+++ frontend/src/components/atoms/Textarea.tsx
@@ -1,5 +1,9 @@
 export function Textarea() {
     return (
-        <></>
+        <>
+            <label htmlFor={`${name}`} className="text-gray-200 block">{label}</label>
+            <input type="text" id={name} name={name} className="border-2 border-gray-100 rounded-lg p-2"
+                   placeholder="Text"/>
+        </>
     );
 }
