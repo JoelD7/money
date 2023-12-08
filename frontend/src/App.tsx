60d8ad521147bf1f994b76053477fdffdc18c38e
JoelD7 <joeld200013@gmail.com>
2023-12-08 19:33:09 -0400

    Delete SelectCustom component
    Why: There is no need for this component to be custom. The Select component from Material UI is enough

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
 import "tailwindcss/tailwind.css";
-import {Tag} from "./components/atoms/Tag.tsx";
-import {Textarea} from "./components/atoms/Textarea.tsx";
-import {SelectCustom} from "./components/atoms/SelectCustom.tsx";
 import {createTheme, ThemeProvider} from "@mui/material";
 
 function App() {
-    const values: string[] = ["Value 1", "Value 2", "Very long value"];
     const theme = createTheme({
         typography: {
             fontFamily: [
@@ -19,7 +15,7 @@ function App() {
             <ThemeProvider theme={theme}>
                 <div style={{width: "1290px"}}>
                     <div className="ml-2" style={{width: "fit-content"}}>
-                        <SelectCustom name="select" label="Currency" values={values}></SelectCustom>
+
                     </div>
                 </div>
             </ThemeProvider>
