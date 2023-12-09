12c881e4cb1e3cd2cf8b36dad673add219e8909e
JoelD7 <joeld200013@gmail.com>
2023-12-09 11:35:39 -0400

    Finish implementation of ChipSelect component

--- frontend/src/components/molecules/ChipSelect.tsx
+++ frontend/src/components/molecules/ChipSelect.tsx
@@ -1,5 +1,6 @@
-import {Box, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent} from "@mui/material";
 import {v4 as uuidv4} from 'uuid';
+import {useState} from "react";
 
 type option = {
     label: string;
@@ -13,6 +14,28 @@ type ChipSelectProps = {
 
 export function ChipSelect({options, label}: ChipSelectProps) {
     const labelId: string = uuidv4();
+    const [selected, setSelected] = useState<string[]>([]);
+    const colorMap: Map<string, string> = buildColorMap();
+
+    function onSelectedChange(event: SelectChangeEvent<typeof selected>) {
+        const {target: {value}} = event;
+        setSelected(
+            typeof value === 'string' ? value.split(' ') : value,
+        );
+    }
+
+    function buildColorMap(): Map<string, string> {
+        const colorMap = new Map<string, string>();
+        options.forEach((option) => {
+            colorMap.set(option.label, option.color);
+        });
+
+        return colorMap;
+    }
+
+    function getOptionColor(value: string): string {
+        return colorMap.get(value) || "gray.main";
+    }
 
     return (
         <>
@@ -22,9 +45,23 @@ export function ChipSelect({options, label}: ChipSelectProps) {
                     labelId={labelId}
                     id={label}
                     label={label}
+                    value={selected}
+                    onChange={onSelectedChange}
                     multiple
+                    renderValue={(selected) => (
+                        // This is how items will appear on the select input
+                        <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
+                            {selected.map((value) => (
+                                <Box className="p-1 w-fit text-sm rounded-xl" style={{color: "white"}}
+                                     sx={{backgroundColor: getOptionColor(value)}}>
+                                    {value}
+                                </Box>
+                            ))}
+                        </Box>
+                    )}
                 >
                     {
+                        // This is how items will appear on the menu
                         options.map((option) => {
                             return (
                                 <MenuItem id={option.label} value={option.label}>
