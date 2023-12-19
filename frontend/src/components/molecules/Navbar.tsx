799b387a03a810d81ac2f7aa230df0a6bd75cdd2
JoelD7 <joeld200013@gmail.com>
2023-12-18 22:00:42 -0400

    Setup icon size to 28px in Navbar

--- frontend/src/components/molecules/Navbar.tsx
+++ frontend/src/components/molecules/Navbar.tsx
@@ -8,38 +8,43 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
 
 export function Navbar() {
+    const customWidth = {
+        '&.MuiSvgIcon-root': {
+            width: "28px",
+            height: "28px",
+        },
+    }
     return (
         <>
-            <nav className="flex flex-col items-center border-black border-2 w-28">
+            <nav className="flex flex-col items-center h-screen border-black border-2 w-28">
                 <img className="mb-10 w-14"
                      src="https://money-static-files.s3.amazonaws.com/images/dollar.png"
                      alt="dollar_logo"/>
 
                 <IconButton>
-
-                    <HomeIcon sx={{
-                        '& .MuiSvgIcon-fontSizeMedium.MuiSvgIcon-root': {
-                            width: "52px",
-                        },
-                    }}/>
+                    <HomeIcon sx={customWidth}/>
                 </IconButton>
                 <IconButton size="medium">
-                    <AccessTimeFilledIcon fontSize="medium"/>
+                    <AccessTimeFilledIcon sx={customWidth}/>
                 </IconButton>
                 <IconButton size="medium">
-                    <NotificationImportantIcon fontSize="medium"/>
+                    <NotificationImportantIcon sx={customWidth}/>
                 </IconButton>
                 <IconButton size="medium">
-                    <LabelIcon fontSize="medium"/>
+                    <LabelIcon sx={customWidth}/>
                 </IconButton>
                 <IconButton size="medium">
-                    <AutoStoriesIcon fontSize="medium"/>
+                    <AutoStoriesIcon sx={customWidth}/>
                 </IconButton>
+                <hr style={{color: "gray.main"}}/>
                 <IconButton size="medium">
-                    <SettingsIcon fontSize="medium"/>
+                    <SettingsIcon sx={customWidth}/>
                 </IconButton>
-                <IconButton size="medium">
-                    <LogoutIcon fontSize="medium"/>
+                <IconButton sx={{
+                    marginTop: "auto",
+                    marginBottom: "20px",
+                }} size="medium">
+                    <LogoutIcon sx={customWidth}/>
                 </IconButton>
             </nav>
 
