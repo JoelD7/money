310a3e7d8433b23ecace3363b3f824118898198f
JoelD7 <joeld200013@gmail.com>
2023-12-20 18:48:36 -0400

    Add tooltips to navbar buttons

--- frontend/src/components/molecules/Navbar.tsx
+++ frontend/src/components/molecules/Navbar.tsx
@@ -1,4 +1,4 @@
-import {IconButton} from "@mui/material";
+import {Divider, IconButton, Tooltip} from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 import NotificationImportantIcon from '@mui/icons-material/NotificationImportant';
@@ -16,36 +16,57 @@ export function Navbar() {
     }
     return (
         <>
-            <nav className="flex flex-col items-center h-screen border-black border-2 w-28">
+            <nav className="flex flex-col items-center h-screen  w-28">
                 <img className="mb-10 w-14"
                      src="https://money-static-files.s3.amazonaws.com/images/dollar.png"
                      alt="dollar_logo"/>
 
-                <IconButton>
-                    <HomeIcon sx={customWidth}/>
-                </IconButton>
-                <IconButton size="medium">
-                    <AccessTimeFilledIcon sx={customWidth}/>
-                </IconButton>
-                <IconButton size="medium">
-                    <NotificationImportantIcon sx={customWidth}/>
-                </IconButton>
-                <IconButton size="medium">
-                    <LabelIcon sx={customWidth}/>
-                </IconButton>
-                <IconButton size="medium">
-                    <AutoStoriesIcon sx={customWidth}/>
-                </IconButton>
-                <hr style={{color: "gray.main"}}/>
-                <IconButton size="medium">
-                    <SettingsIcon sx={customWidth}/>
-                </IconButton>
-                <IconButton sx={{
-                    marginTop: "auto",
-                    marginBottom: "20px",
-                }} size="medium">
-                    <LogoutIcon sx={customWidth}/>
-                </IconButton>
+                <Tooltip title="Home" placement="right">
+                    <IconButton sx={{margin: "5px 0px"}}>
+                        <HomeIcon sx={customWidth}/>
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="History" placement="right">
+                    <IconButton sx={{margin: "5px 0px"}}>
+                        <AccessTimeFilledIcon sx={customWidth}/>
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Notifications" placement="right">
+                    <IconButton sx={{margin: "5px 0px"}}>
+                        <NotificationImportantIcon sx={customWidth}/>
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Categories" placement="right">
+                    <IconButton sx={{margin: "5px 0px"}}>
+                        <LabelIcon sx={customWidth}/>
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Savings" placement="right">
+                    <IconButton sx={{margin: "5px 0px"}}>
+                        <AutoStoriesIcon sx={customWidth}/>
+                    </IconButton>
+                </Tooltip>
+
+                <Divider sx={{width: "60%", margin: "20px 0px"}}/>
+
+                <Tooltip title="Settings" placement="right">
+                    <IconButton>
+                        <SettingsIcon sx={customWidth}/>
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Logout" placement="right">
+                    <IconButton sx={{
+                        marginTop: "auto",
+                        marginBottom: "20px",
+                    }}>
+                        <LogoutIcon sx={customWidth}/>
+                    </IconButton>
+                </Tooltip>
             </nav>
 
         </>
