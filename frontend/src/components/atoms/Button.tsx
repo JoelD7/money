6527321372474e415f5c48577b1f5ef64e567b40
JoelD7 <joeld200013@gmail.com>
2024-01-03 20:06:22 -0400

    Update custom Button component so that it uses Material UI's button underneath
    Why:
     - Decided to use Material UI instead of creating custom components from scratch
     - There are some common CSS props that I want all my buttons to have (as of this moment: textTransform and borderRadius), so it made sense to create a component instead of having to repeat thoe every time

--- frontend/src/components/atoms/Button.tsx
+++ frontend/src/components/atoms/Button.tsx
@@ -1,39 +1,16 @@
-type ButtonProps = {
-    color: ButtonColor;
-    text: string;
-};
+import {Button as MuiButton, ButtonProps} from "@mui/material";
+import {ReactNode} from "react";
 
-export enum ButtonColor {
-    Red = "RED",
-    Orange = "ORANGE",
-    Green = "GREEN",
-    Gray = "GRAY",
-    White = "WHITE",
-}
+type CustomButtonProps = {
+    children: ReactNode
+} & ButtonProps
 
-export function Button({color = ButtonColor.White, text = "Button"}: ButtonProps) {
+export function Button(props: CustomButtonProps) {
     return (
         <>
-            <button className={`${getButtonColorClass(color)} shadow-lg`}>
-                {text}
-            </button>
+            <MuiButton sx={{textTransform: "capitalize", borderRadius: "1rem"}} {...props}>
+                {props.children}
+            </MuiButton>
         </>
     );
-}
-
-function getButtonColorClass(color: ButtonColor): string {
-    switch (color) {
-        case ButtonColor.Red:
-            return "bg-red-100 hover:bg-red-200 active:bg-red-300";
-        case ButtonColor.Orange:
-            return "bg-orange-100 hover:bg-orange-200 active:bg-orange-300";
-        case ButtonColor.Green:
-            return "bg-green-100 hover:bg-green-200 active:bg-green-300";
-        case ButtonColor.Gray:
-            return "bg-gray-100 hover:bg-gray-200 active:bg-gray-300";
-        case ButtonColor.White:
-            return "border-green-300 border-2 bg-white-100 hover:bg-white-200 active:bg-white-300";
-        default:
-            return "bg-green-100 hover:bg-green-200 active:bg-green-300";
-    }
 }
