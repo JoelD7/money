2771aa692d8314bc513f33e4f7c029d2eca47888
JoelD7 <joeld200013@gmail.com>
2024-02-16 12:14:11 -0400

    Remove use of exponential backoff from custom redis client
    Why: To avoid lambda timeouts, and because the go-redis library already does it. Details here: https://www.notion.so/Not-using-custom-exponential-backoff-for-Redis-e569def3627f4e9d811f9630b00e53ca?pvs=4

--- backend/storage/cache/redis_client.go
+++ backend/storage/cache/redis_client.go
@@ -3,7 +3,6 @@ package cache
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/JoelD7/money/backend/models"
 	"github.com/JoelD7/money/backend/shared/env"
 	"github.com/redis/go-redis/v9"
@@ -11,15 +10,9 @@ import (
 )
 
 var (
-	client      *redis.Client
 	redisClient RedisAPI
 
 	redisURL = env.GetString("REDIS_URL", "redis://default:810cc997ccd745debfbbdb567631a5c2@us1-polished-shrew-39844.upstash.io:39844")
-)
-
-const (
-	retries       = 3
-	backoffFactor = 2
 )
 
 type RedisAPI interface {
@@ -43,36 +36,16 @@ func init() {
 }
 
 func (rc *RedisClient) Get(ctx context.Context, key string) (string, error) {
-	start := time.Now()
 	value, err := rc.client.Get(ctx, key).Result()
 	if errors.Is(err, redis.Nil) {
 		return "", models.ErrInvalidTokensNotFound
 	}
 
-	backoff := time.Second * 2
-
-	for i := 0; i < retries && err != nil; i++ {
-		time.Sleep(backoff)
-
-		value, err = rc.client.Get(ctx, key).Result()
-		backoff *= backoffFactor
-	}
-
-	fmt.Println("Redis Get Duration: ", time.Since(start))
 	return value, err
 }
 
 func (rc *RedisClient) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
 	_, err := rc.client.Set(ctx, key, value, expiration).Result()
-
-	backoff := time.Second * 2
-
-	for i := 0; i < retries && err != nil; i++ {
-		time.Sleep(backoff)
-
-		_, err = rc.client.Set(ctx, key, value, expiration).Result()
-		backoff *= backoffFactor
-	}
 
 	return err
 }
