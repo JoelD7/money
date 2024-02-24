01bf0c049442cfe677d05ece4d217e2e340e402d
JoelD7 <joeld200013@gmail.com>
2024-02-23 20:16:37 -0400

    [income] Use router's logger

--- backend/shared/logger/logger.go
+++ backend/shared/logger/logger.go
@@ -30,7 +30,7 @@ const (
 
 var (
 	logstashServerType = env.GetString("LOGSTASH_TYPE", "tcp")
-	logstashHost       = env.GetString("LOGSTASH_HOST", "ec2-54-165-24-229.compute-1.amazonaws.com")
+	logstashHost       = env.GetString("LOGSTASH_HOST", "ec2-3-87-238-170.compute-1.amazonaws.com")
 	logstashPort       = env.GetString("LOGSTASH_PORT", "5044")
 
 	stackCleaner = regexp.MustCompile(`[^\t]*:\d+`)
