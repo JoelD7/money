92243a8e881ec51b5fd47520ba98ade492c4e669
JoelD7 <joeld200013@gmail.com>
2024-03-06 20:24:49 -0400

    [Button] Add loading animation

--- frontend/src/api/money-api.ts
+++ frontend/src/api/money-api.ts
@@ -3,7 +3,5 @@ import axios from "axios"
 export const BASE_URL = "https://38qslpe8d9.execute-api.us-east-1.amazonaws.com/staging"
 
 export function signUp(newUser: SignUpUser){
-    return axios.post(BASE_URL+"/auth/signup", newUser, {
-
-    })
+    return axios.post(BASE_URL+"/auth/signup", newUser)
 }
--- frontend/src/components/atoms/Button.tsx
+++ frontend/src/components/atoms/Button.tsx
@@ -1,36 +1,56 @@
-import {Button as MuiButton, ButtonProps, SxProps, Theme} from "@mui/material";
-import {ReactNode} from "react";
+import {
+  Button as MuiButton,
+  ButtonProps,
+  SxProps,
+  Theme,
+} from "@mui/material";
+import { ReactNode } from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SaveIcon from "@mui/icons-material/Save";
 
 type CustomButtonProps = {
-    children: ReactNode
-} & ButtonProps
+  children: ReactNode;
+  loading?: boolean;
+} & ButtonProps;
 
 export function Button(props: CustomButtonProps) {
-    const {sx, variant, ...other} = props
-    let styles: SxProps<Theme> | undefined = {
-        textTransform: "capitalize",
-        borderRadius: "1rem",
-        ...sx
-    }
+  const { sx, loading, variant, children, ...other } = props;
+  let styles: SxProps<Theme> | undefined = {
+    textTransform: "capitalize",
+    borderRadius: "1rem",
+    ...sx,
+  };
 
-    if (variant === "outlined") {
-        styles = {
-            ...styles,
-            '&.MuiButton-root': {
-                backgroundColor: "#ffffff",
-            },
-        }
-    }
+  if (variant === "outlined") {
+    styles = {
+      ...styles,
+      "&.MuiButton-root": {
+        backgroundColor: "#ffffff",
+      },
+    };
+  }
 
-    return (
-        <>
-            <MuiButton
-                sx={styles}
-                /*@ts-ignore*/
-                variant={variant}
-                {...other}>
-                {props.children}
-            </MuiButton>
-        </>
-    );
+  return (
+    <>
+      {loading ? (
+        <LoadingButton
+          sx={styles}
+          loading
+          loadingPosition="start"
+          startIcon={<SaveIcon />}
+          variant={variant}
+          {...other}
+        ></LoadingButton>
+      ) : (
+        <MuiButton
+          sx={styles}
+          /*@ts-ignore*/
+          variant={variant}
+          {...other}
+        >
+          {children}
+        </MuiButton>
+      )}
+    </>
+  );
 }
