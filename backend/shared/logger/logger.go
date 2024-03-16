9364a8e94ea8866131671dab68091d452716fb75
JoelD7 <joeld200013@gmail.com>
2024-03-15 20:50:03 -0400

    Remove optionsHandler for /signup
    Why: I configured CORS in API Gateway directly

--- backend/shared/logger/logger.go
+++ backend/shared/logger/logger.go
@@ -31,7 +31,7 @@ const (
 
 var (
 	logstashServerType = env.GetString("LOGSTASH_TYPE", "tcp")
-	logstashHost       = env.GetString("LOGSTASH_HOST", "ec2-54-226-174-190.compute-1.amazonaws.com")
+	logstashHost       = env.GetString("LOGSTASH_HOST", "ec2-54-226-32-165.compute-1.amazonaws.com")
 	logstashPort       = env.GetString("LOGSTASH_PORT", "5044")
 
 	stackCleaner = regexp.MustCompile(`[^\t]*:\d+`)
