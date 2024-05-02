2fba72de849869c4fbf8595f02ec88bc4117772f
JoelD7 <joeld200013@gmail.com>
2024-05-02 09:51:52 -0400

    [frontend] Load environment variables using Vite
    Why: Despite showing no errors on the IDE, the compiled code on the browser didn't find the env vars

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+
+// Cherry-pick the keys you want to expose to the client
+const cherryPickedKeys = ["REACT_APP_API_BASE_URL"];
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),  TanStackRouterVite(),],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv = {};
+  cherryPickedKeys.forEach(key => processEnv[key] = env[key]);
+
+  return {
+    define: {
+      'process.env': processEnv
+    },
+    plugins: [react(),TanStackRouterVite()],
+  }
 })
