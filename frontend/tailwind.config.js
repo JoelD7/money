ced1c49f2cfa06eec57441cbb3296499c9e086eb
JoelD7 <joeld200013@gmail.com>
2024-05-18 13:10:28 -0400

    [frontend] Add custom breakpoints to tailwind.config.js
    Why: So that tailwind breakpoints match with Material UI's

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,51 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  safelist: [
-    'bg-white',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["bg-white"],
   theme: {
     extend: {},
-    colors:{
+    screens: {
+      sm: "600px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1536px",
+    },
+    colors: {
       white: {
-        100: '#FFFFFF',
-        200: '#e6e6e6',
-        300: '#cccccc',
+        100: "#FFFFFF",
+        200: "#e6e6e6",
+        300: "#cccccc",
       },
       green: {
-        100: '#009821',
-        200: '#00851d',
-        300: '#005212'
+        100: "#009821",
+        200: "#00851d",
+        300: "#005212",
       },
       orange: {
-        100: '#FF8042',
-        200: '#ee6520',
-        300: '#d44c08'
+        100: "#FF8042",
+        200: "#ee6520",
+        300: "#d44c08",
       },
       red: {
-        100: '#D90707',
-        200: '#ad0101',
-        300: '#7a0101',
+        100: "#D90707",
+        200: "#ad0101",
+        300: "#7a0101",
       },
-      darkGreen: '#024511',
+      darkGreen: "#024511",
       blue: {
-        100: '#0088FE',
-        200: '#006dcc',
-        300: '#004d99',
+        100: "#0088FE",
+        200: "#006dcc",
+        300: "#004d99",
       },
-      gray:{
-        100: '#D9D9D9',
-        200: '#6F6F6F',
-        300: '#4D4D4D',
-      }
+      gray: {
+        100: "#D9D9D9",
+        200: "#6F6F6F",
+        300: "#4D4D4D",
+      },
     },
     fontFamily: {
-      sans: ['Outfit', 'sans-serif'],
-    }
+      sans: ["Outfit", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+};
