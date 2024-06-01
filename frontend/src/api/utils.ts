cc178ad7faf0aa996858b0efcbc851c24a7122c4
JoelD7 <joeld200013@gmail.com>
2024-06-01 14:15:38 -0400

    [frontend] Don't retry requests that respond with 400
    Why: A 400 indicates a client error, therefore it is the client that needs to manually perform the request after the correction indicated by the response error is made.

--- frontend/src/api/utils.ts
+++ frontend/src/api/utils.ts
@@ -1,3 +1,5 @@
+import { AxiosError } from "axios";
+
 const BACKOFF_TIME_MS: number = 1000;
 export const MAX_RETRIES: number = 3;
 
@@ -14,8 +16,12 @@ export async function retryableRequest(request: () => Promise<void>) {
       myErr = undefined;
       return;
     } catch (err) {
-      myErr = err;
+      myErr = err as AxiosError;
+      if (myErr.response && myErr.response.status >= 400 && myErr.response.status < 500) {
+        throw myErr;
+      }
     }
+
     await sleep(BACKOFF_TIME_MS);
   }
 
