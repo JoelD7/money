aac30cd054bee46c9618163136fa17db5c680a87
JoelD7 <joeld200013@gmail.com>
2024-06-12 10:03:23 -0400

    [Expenses] Add recurring_day and is_recurring fields to Expense model
    Why: Support recurring expenses

--- backend/models/expense.go
+++ backend/models/expense.go
@@ -8,6 +8,8 @@ type Expense struct {
 	CategoryID   *string   `json:"category_id,omitempty"`
 	CategoryName string    `json:"category_name,omitempty"`
 	Amount       *float64  `json:"amount"`
+	RecurringDay *int      `json:"recurring_day,omitempty"`
+	IsRecurring  *bool     `json:"is_recurring,omitempty"`
 	Name         *string   `json:"name,omitempty"`
 	Notes        string    `json:"notes,omitempty"`
 	CreatedDate  time.Time `json:"created_date,omitempty"`
