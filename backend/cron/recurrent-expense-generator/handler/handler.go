dae1f3453b919e904b164fdbf5aaba37dd90164f
JoelD7 <joeld200013@gmail.com>
2024-06-21 12:14:48 -0400

    [expenses recurring] Create cron lambda that generates recurring expenses
    Why: The lambda will execute daily to create the recurring expenses that need to be created for that day

---
+++ backend/cron/recurrent-expense-generator/handler/handler.go
@@ -0,0 +1,154 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+	"github.com/JoelD7/money/backend/models"
+	"github.com/JoelD7/money/backend/shared/env"
+	"github.com/JoelD7/money/backend/shared/logger"
+	"github.com/JoelD7/money/backend/storage/expenses"
+	expenses_recurring "github.com/JoelD7/money/backend/storage/expenses-recurring"
+	"github.com/JoelD7/money/backend/storage/period"
+	"github.com/JoelD7/money/backend/storage/shared"
+	"github.com/JoelD7/money/backend/usecases"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"sync"
+	"time"
+)
+
+var (
+	awsRegion = env.GetString("REGION", "us-east-1")
+	once      sync.Once
+)
+
+type CronRequest struct {
+	Log          logger.LogAPI
+	Repo         expenses_recurring.Repository
+	PeriodRepo   period.Repository
+	ExpensesRepo expenses.Repository
+
+	err          error
+	startingTime time.Time
+}
+
+func Handle(ctx context.Context) error {
+	req := new(CronRequest)
+	var err error
+
+	stackTrace, ctxError := shared.ExecuteLambda(ctx, func(ctx context.Context) {
+		req.init()
+		defer req.finish()
+
+		err = req.Process(ctx)
+	})
+
+	if ctxError != nil {
+		req.Log.Error("request_timeout", ctxError, []models.LoggerObject{
+			req.Log.MapToLoggerObject("stack", map[string]interface{}{
+				"s_trace": stackTrace,
+			}),
+		})
+	}
+
+	return err
+}
+
+func (req *CronRequest) init() {
+	once.Do(func() {
+		dynamoClient := initDynamoClient()
+		req.Repo = expenses_recurring.NewExpenseRecurringDynamoRepository(dynamoClient)
+		req.PeriodRepo = period.NewDynamoRepository(dynamoClient)
+		req.ExpensesRepo = expenses.NewDynamoRepository(dynamoClient)
+		req.Log = logger.NewLogger()
+	})
+	req.startingTime = time.Now()
+}
+
+func initDynamoClient() *dynamodb.Client {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(awsRegion))
+	if err != nil {
+		panic(err)
+	}
+
+	return dynamodb.NewFromConfig(cfg)
+}
+
+func (req *CronRequest) finish() {
+	defer func() {
+		err := req.Log.Finish()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	req.Log.LogLambdaTime(req.startingTime, req.err, recover())
+}
+
+func (req *CronRequest) Process(ctx context.Context) error {
+	day := time.Now().Day()
+
+	recExpenses, err := req.Repo.ScanExpensesForDay(ctx, day)
+	if err != nil {
+		req.err = err
+		req.Log.Error("scan_expenses_for_day_failed", err, []models.LoggerObject{
+			req.Log.MapToLoggerObject("run_information", map[string]interface{}{
+				"i_day": day,
+			}),
+		})
+
+		return err
+	}
+
+	recExpensesByUser := make(map[string][]*models.ExpenseRecurring)
+	for _, expense := range recExpenses {
+		recExpensesByUser[expense.Username] = append(recExpensesByUser[expense.Username], expense)
+	}
+
+	for username, userRecurringExpenses := range recExpensesByUser {
+		err = req.createExpenses(ctx, username, userRecurringExpenses)
+		if err != nil {
+			req.Log.Error("create_expenses_failed", err, []models.LoggerObject{
+				req.Log.MapToLoggerObject("run_information", map[string]interface{}{
+					"s_username": username,
+				}),
+			})
+		}
+	}
+
+	return nil
+}
+
+func (req *CronRequest) createExpenses(ctx context.Context, username string, recExpenses []*models.ExpenseRecurring) error {
+	lastPeriod, err := req.PeriodRepo.GetLastPeriod(ctx, username)
+	if err != nil {
+		return fmt.Errorf("get last period failed: %v", err)
+	}
+
+	expensesToCreate := make([]*models.Expense, 0, len(recExpenses))
+
+	for _, expense := range recExpenses {
+		if isRecurringExpenseWithinPeriod(expense, lastPeriod) {
+			expensesToCreate = append(expensesToCreate, &models.Expense{
+				Username:   expense.Username,
+				CategoryID: expense.CategoryID,
+				Amount:     &expense.Amount,
+				Name:       &expense.Name,
+				Notes:      expense.Notes,
+				Period:     lastPeriod.Name,
+			})
+		}
+	}
+
+	batchCreateExpenses := usecases.NewBatchExpensesCreator(req.ExpensesRepo, req.Log)
+	err = batchCreateExpenses(ctx, expensesToCreate)
+	if err != nil {
+		return fmt.Errorf("batch create expenses failed: %v", err)
+	}
+
+	return nil
+}
+
+func isRecurringExpenseWithinPeriod(re *models.ExpenseRecurring, p *models.Period) bool {
+	return (re.CreatedDate.After(p.StartDate) || re.CreatedDate.Equal(p.StartDate)) && (re.CreatedDate.Before(p.EndDate) || re.CreatedDate.Equal(p.EndDate))
+}
