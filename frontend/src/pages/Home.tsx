585e8718329c670306248fcc41fd628b524619c3
JoelD7 <joeld200013@gmail.com>
2024-08-03 15:54:26 -0400

    [frontend] Use custom hooks for Tanstack Query

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,26 +1,24 @@
-import {
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import { Typography, useMediaQuery, useTheme } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import AddIcon from "@mui/icons-material/Add";
 import {
   BackgroundRefetchErrorSnackbar,
   BalanceCard,
-  Button, Container,
+  Button,
+  Container,
   ExpenseCard,
   ExpensesChart,
-  ExpensesTable, LinearProgress, Navbar,
+  ExpensesTable,
+  LinearProgress,
+  Navbar,
   NewExpense,
 } from "../components";
 import { Expense, Period, User } from "../types";
-import { useQuery } from "@tanstack/react-query";
-import api from "../api";
 import { Loading } from "./Loading.tsx";
 import { Error } from "./Error.tsx";
 import { Colors } from "../assets";
 import { useState } from "react";
+import { useGetExpenses, useGetPeriod, useGetUser } from "./queries.ts";
 
 type CategoryExpense = {
   category: string;
@@ -35,20 +33,9 @@ export function Home() {
 
   const mdUp: boolean = useMediaQuery(theme.breakpoints.up("md"));
 
-  const getUser = useQuery({
-    queryKey: ["user"],
-    queryFn: () => api.getUser(),
-  });
-
-  const getExpenses = useQuery({
-    queryKey: ["expenses"],
-    queryFn: () => api.getExpenses(),
-  });
-
-  const getPeriod = useQuery({
-    queryKey: ["period"],
-    queryFn: () => api.getPeriod(),
-  });
+  const getUser = useGetUser();
+  const getExpenses = useGetExpenses();
+  const getPeriod = useGetPeriod();
 
   const user: User | undefined = getUser.data?.data;
   const expenses: Expense[] | undefined = getExpenses.data?.data.expenses;
@@ -121,94 +108,93 @@ export function Home() {
   }
 
   return (
-      <Container>
-        <BackgroundRefetchErrorSnackbar/>
-        <LinearProgress loading={getUser.isFetching || getExpenses.isFetching || getPeriod.isFetching}/>
-        <Navbar/>
+    <Container>
+      <BackgroundRefetchErrorSnackbar />
+      <LinearProgress
+        loading={
+          getUser.isFetching || getExpenses.isFetching || getPeriod.isFetching
+        }
+      />
+      <Navbar />
 
-        <Grid
-            container
-            justifyContent={"center"}
-            position={"relative"}
-        >
-
-          {/*Balance*/}
-          <Grid xs={12} sm={6} hidden={mdUp}>
-            <BalanceCard remainder={user ? user.remainder : 0}/>
-          </Grid>
-
-          {/*Expenses*/}
-          <Grid xs={12} sm={6} hidden={mdUp}>
-            <ExpenseCard expenses={user ? user.expenses : 0}/>
-          </Grid>
-
-          {/*Chart, Current balance and expenses*/}
-          <Grid xs={12} maxWidth={"1200px"}>
-            <div>
-              <Grid container spacing={1}>
-                {/*Chart section*/}
-                <Grid xs={12} md={6}>
-                  <ExpensesChart
-                      period={period}
-                      categoryExpense={categoryExpense}
-                      chartHeight={chartHeight}
-                      isLoading={getUser.isLoading || getExpenses.isLoading}
-                      isError={getExpenses.isError}
-                  />
-                </Grid>
-                {/*New expense/income buttons, Current balance and expenses*/}
-                <Grid xs={12} md={6}>
-                  <div>
-                    <Grid container mt={"1rem"} spacing={1}>
-                      {/*Balance*/}
-                      <Grid xs={12} hidden={!mdUp}>
-                        <BalanceCard remainder={user ? user.remainder : 0}/>
-                      </Grid>
-
-                      {/*Expenses*/}
-                      <Grid xs={12} hidden={!mdUp}>
-                        <ExpenseCard expenses={user ? user.expenses : 0}/>
-                      </Grid>
-
-                      {/**New expense/income buttons*/}
-                      <Grid xs={12}>
-                        <Button
-                            color={"secondary"}
-                            variant={"contained"}
-                            startIcon={<AddIcon/>}
-                            onClick={() => setOpenNewExpense(true)}
-                        >
-                          New expense
-                        </Button>
-
-                        <Button
-                            sx={{marginLeft: "1rem"}}
-                            variant={"contained"}
-                            startIcon={<AddIcon/>}
-                        >
-                          New income
-                        </Button>
-                      </Grid>
-                    </Grid>
-                  </div>
-                </Grid>
-              </Grid>
-            </div>
-          </Grid>
-
-          {/*Latest table*/}
-          <Grid xs={12} maxWidth={"1200px"}>
-            <Typography mt={"2rem"} variant={"h4"}>
-              Latest
-            </Typography>
-
-            {expenses && user && user.categories && (
-                <ExpensesTable expenses={expenses} categories={user.categories}/>
-            )}
-          </Grid>
+      <Grid container justifyContent={"center"} position={"relative"}>
+        {/*Balance*/}
+        <Grid xs={12} sm={6} hidden={mdUp}>
+          <BalanceCard remainder={user ? user.remainder : 0} />
         </Grid>
 
-        <NewExpense open={openNewExpense} onClose={handleClose}/>
-      </Container>
+        {/*Expenses*/}
+        <Grid xs={12} sm={6} hidden={mdUp}>
+          <ExpenseCard expenses={user ? user.expenses : 0} />
+        </Grid>
+
+        {/*Chart, Current balance and expenses*/}
+        <Grid xs={12} maxWidth={"1200px"}>
+          <div>
+            <Grid container spacing={1}>
+              {/*Chart section*/}
+              <Grid xs={12} md={6}>
+                <ExpensesChart
+                  period={period}
+                  categoryExpense={categoryExpense}
+                  chartHeight={chartHeight}
+                  isLoading={getUser.isLoading || getExpenses.isLoading}
+                  isError={getExpenses.isError}
+                />
+              </Grid>
+              {/*New expense/income buttons, Current balance and expenses*/}
+              <Grid xs={12} md={6}>
+                <div>
+                  <Grid container mt={"1rem"} spacing={1}>
+                    {/*Balance*/}
+                    <Grid xs={12} hidden={!mdUp}>
+                      <BalanceCard remainder={user ? user.remainder : 0} />
+                    </Grid>
+
+                    {/*Expenses*/}
+                    <Grid xs={12} hidden={!mdUp}>
+                      <ExpenseCard expenses={user ? user.expenses : 0} />
+                    </Grid>
+
+                    {/**New expense/income buttons*/}
+                    <Grid xs={12}>
+                      <Button
+                        color={"secondary"}
+                        variant={"contained"}
+                        startIcon={<AddIcon />}
+                        onClick={() => setOpenNewExpense(true)}
+                      >
+                        New expense
+                      </Button>
+
+                      <Button
+                        sx={{ marginLeft: "1rem" }}
+                        variant={"contained"}
+                        startIcon={<AddIcon />}
+                      >
+                        New income
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+
+        {/*Latest table*/}
+        <Grid xs={12} maxWidth={"1200px"}>
+          <Typography mt={"2rem"} variant={"h4"}>
+            Latest
+          </Typography>
+
+          {expenses && user && user.categories && (
+            <ExpensesTable expenses={expenses} categories={user.categories} />
+          )}
+        </Grid>
+      </Grid>
+
+      <NewExpense open={openNewExpense} onClose={handleClose} />
+    </Container>
   );
 }
