de11bb2a3c7a3dae41aac739d65cb1cdea9f64e6
JoelD7 <joeld200013@gmail.com>
2024-09-30 10:16:06 -0400

    [IncomeCard] Add loading animation

--- frontend/src/components/molecules/IncomeCard.tsx
+++ frontend/src/components/molecules/IncomeCard.tsx
@@ -1,18 +1,21 @@
 import Grid from "@mui/material/Unstable_Grid2";
 import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
-import { Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Skeleton, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Colors } from "../../assets";
 
 type IncomeCardProps = {
   income?: number;
+  loading: boolean;
 };
 
-export function IncomeCard({ income }: IncomeCardProps) {
+export function IncomeCard({ income, loading }: IncomeCardProps) {
   const theme = useTheme();
   const xsOnly: boolean = useMediaQuery(theme.breakpoints.only("xs"));
   const customWidth = {
     "&.MuiSvgIcon-root": {
       width: "38px",
       height: "38px",
+      color: Colors.GREEN,
     },
   };
 
@@ -26,29 +29,55 @@ export function IncomeCard({ income }: IncomeCardProps) {
         bgcolor="white.main"
         boxShadow={"2"}
       >
-        <Grid xs={3}>
-          <Grid
-            height="100%"
-            container
-            alignContent="center"
-            justifyContent="center"
-          >
-            {/*@ts-ignore*/}
-            <ArrowCircleRightOutlinedIcon sx={customWidth} color="red" />
-          </Grid>
-        </Grid>
+        {loading ? (
+          <>
+            <Grid xs={3}>
+              <Grid
+                height="100%"
+                container
+                alignContent="center"
+                justifyContent="center"
+              >
+                <Skeleton variant={"circular"} width={40} height={40} />
+              </Grid>
+            </Grid>
 
-        <Grid xs={9}>
-          <Typography variant="h6" fontWeight="bold">
-            Income
-          </Typography>
-          <Typography lineHeight="unset" variant="h4" color="red.main">
-            {new Intl.NumberFormat("en-US", {
-              style: "currency",
-              currency: "USD",
-            }).format(income ? income : 0)}
-          </Typography>
-        </Grid>
+            <Grid xs={9}>
+              <Skeleton
+                variant={"rectangular"}
+                sx={{ marginBottom: "5px" }}
+                width={282}
+                height={31}
+              />
+              <Skeleton variant={"rectangular"} width={282} height={50} />
+            </Grid>
+          </>
+        ) : (
+          <>
+            <Grid xs={3}>
+              <Grid
+                height="100%"
+                container
+                alignContent="center"
+                justifyContent="center"
+              >
+                <ArrowCircleRightOutlinedIcon sx={customWidth} />
+              </Grid>
+            </Grid>
+
+            <Grid xs={9}>
+              <Typography variant="h6" fontWeight="bold">
+                Income
+              </Typography>
+              <Typography lineHeight="unset" variant="h4" color="primary">
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(income ? income : 0)}
+              </Typography>
+            </Grid>
+          </>
+        )}
       </Grid>
     </div>
   );
