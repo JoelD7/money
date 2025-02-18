ee178b4d992c5b30ab6b20e49447f0fd23d79983
JoelD7 <joeld200013@gmail.com>
2025-02-18 11:17:25 -0400

    [frontend] Create custom DataGrid component: Table.tsx
    Why: Facilitate the reuse of the same design and logic of the DataGrid

---
+++ frontend/src/components/molecules/Table.tsx
@@ -0,0 +1,50 @@
+import { DataGrid, DataGridProps } from "@mui/x-data-grid";
+import { NoRowsDataGrid } from "../atoms";
+
+type CustomDataGridProps = {
+  noRowsMessage?: string;
+} & DataGridProps;
+
+export function Table({ noRowsMessage, ...props }: CustomDataGridProps) {
+  const gridStyle = {
+    "&.MuiDataGrid-root": {
+      borderRadius: "1rem",
+      backgroundColor: "#ffffff",
+      minHeight: "220px",
+    },
+    "&.MuiDataGrid-root .MuiDataGrid-cellContent": {
+      textWrap: "pretty",
+      maxHeight: "38px",
+    },
+    "& .MuiDataGrid-columnHeaderTitle": {
+      fontSize: "large",
+    },
+  };
+
+  function NoGrid() {
+    return (
+      <NoRowsDataGrid message={noRowsMessage ? noRowsMessage : "No data available"} />
+    );
+  }
+
+  return (
+    <DataGrid
+      sx={gridStyle}
+      {...props}
+      slots={{
+        noRowsOverlay: NoGrid,
+      }}
+      slotProps={{
+        noRowsOverlay: {
+          sx: {
+            height: "100px",
+          },
+        },
+        loadingOverlay: {
+          variant: "linear-progress",
+          noRowsVariant: "skeleton",
+        },
+      }}
+    />
+  );
+}
--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -3,4 +3,5 @@ export { BalanceCard } from "./BalanceCard";
 export { ExpenseCard } from "./ExpenseCard";
 export { ErrorSnackbar } from "./ErrorSnackbar";
 export { BackgroundRefetchErrorSnackbar } from "./BackgroundRefetchErrorSnackbar";
-export { IncomeCard } from './IncomeCard'
+export { IncomeCard } from "./IncomeCard";
+export { Table } from "./Table";
