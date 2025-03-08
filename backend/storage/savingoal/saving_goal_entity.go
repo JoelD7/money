5f4c48af22f416e807696b6f5e8885e7df5c6ab9
JoelD7 <joeld200013@gmail.com>
2025-03-08 16:14:04 -0400

    [saving goal api] Add fields "isRecurring" and "recurringAmount" to saving goal

--- backend/storage/savingoal/saving_goal_entity.go
+++ backend/storage/savingoal/saving_goal_entity.go
@@ -6,24 +6,28 @@ import (
 )
 
 type savingGoalEntity struct {
-	SavingGoalID string     `json:"saving_goal_id,omitempty" dynamodbav:"saving_goal_id"`
-	Username     string     `json:"username,omitempty" dynamodbav:"username"`
-	Name         string     `json:"name,omitempty" dynamodbav:"name"`
-	Target       float64    `json:"target,omitempty" dynamodbav:"target"`
-	Progress     float64    `json:"progress,omitempty" dynamodbav:"progress"`
-	Deadline     time.Time  `json:"deadline,omitempty" dynamodbav:"deadline"`
-	CreatedAt    *time.Time `json:"created_at,omitempty" dynamodbav:"created_at"`
-	UpdatedAt    *time.Time `json:"updated_at,omitempty" dynamodbav:"updated_at"`
+	SavingGoalID    string     `json:"saving_goal_id,omitempty" dynamodbav:"saving_goal_id"`
+	Username        string     `json:"username,omitempty" dynamodbav:"username"`
+	Name            string     `json:"name,omitempty" dynamodbav:"name"`
+	Target          float64    `json:"target,omitempty" dynamodbav:"target"`
+	Progress        float64    `json:"progress,omitempty" dynamodbav:"progress"`
+	Deadline        time.Time  `json:"deadline,omitempty" dynamodbav:"deadline"`
+	CreatedAt       *time.Time `json:"created_at,omitempty" dynamodbav:"created_at"`
+	UpdatedAt       *time.Time `json:"updated_at,omitempty" dynamodbav:"updated_at"`
+	IsRecurring     bool       `json:"is_recurring,omitempty" dynamodbav:"is_recurring"`
+	RecurringAmount float64    `json:"recurring_amount,omitempty" dynamodbav:"recurring_amount"`
 }
 
 func toSavingGoalEntity(s *models.SavingGoal) *savingGoalEntity {
 	return &savingGoalEntity{
-		SavingGoalID: s.GetSavingGoalID(),
-		Username:     s.GetUsername(),
-		Name:         s.GetName(),
-		Target:       s.GetTarget(),
-		Progress:     s.GetProgress(),
-		Deadline:     s.GetDeadline(),
+		SavingGoalID:    s.GetSavingGoalID(),
+		Username:        s.GetUsername(),
+		Name:            s.GetName(),
+		Target:          s.GetTarget(),
+		Progress:        s.GetProgress(),
+		Deadline:        s.GetDeadline(),
+		IsRecurring:     s.GetIsRecurring(),
+		RecurringAmount: s.GetRecurringAmount(),
 	}
 }
 
@@ -32,14 +36,17 @@ func toSavingGoalModel(s *savingGoalEntity) *models.SavingGoal {
 	deadlinePtr := &s.Deadline
 	targetPtr := &s.Target
 	progressPtr := &s.Progress
+	recurringAmountPtr := &s.RecurringAmount
 
 	return &models.SavingGoal{
-		SavingGoalID: s.SavingGoalID,
-		Username:     s.Username,
-		Name:         namePtr,
-		Target:       targetPtr,
-		Progress:     progressPtr,
-		Deadline:     deadlinePtr,
+		SavingGoalID:    s.SavingGoalID,
+		Username:        s.Username,
+		Name:            namePtr,
+		Target:          targetPtr,
+		Progress:        progressPtr,
+		Deadline:        deadlinePtr,
+		IsRecurring:     s.IsRecurring,
+		RecurringAmount: recurringAmountPtr,
 	}
 }
 
